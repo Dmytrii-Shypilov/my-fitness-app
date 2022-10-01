45d42ac064a23c537237b9526d2ac3ad9288c379
Dmytrii Shypilov <95936002+Dmytrii-Shypilov@users.noreply.github.com>
2022-10-01 22:48:33 +0300

    commit before merge

--- src/components/Modal/DescriptionBlock/DescriptionBlock.jsx
+++ src/components/Modal/DescriptionBlock/DescriptionBlock.jsx
@@ -1,11 +1,18 @@
 import s from './description-block.module.scss'
-
+import { useSelector, shallowEqual } from 'react-redux'
+import { getTrainings } from 'redux/trainings/trainings-selector'
 
 const DescriptionBlock = ({goBack}) => {
+  const trainings = useSelector(getTrainings, shallowEqual)
     return (
-      <div>
-        <div>
-            <button onClick={goBack} >Back</button>
+      <div className={s.listBlock}>
+        <ul className={s.list}>
+          <li className={s.listItem}>
+            <span className={s.name}></span><span className={s.parameters}></span> 
+          </li>
+        </ul>
+        <div className={s.btnContainer}>
+            <button className={s.btn} onClick={goBack} >Back</button>
         </div>
       </div>  
     )
